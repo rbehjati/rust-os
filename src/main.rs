d19db60a0979c3ed636e6dadf0cb0a7591a1a892
Razieh Behjati <razieh@google.com>
2022-03-24 09:28:21 +0000

    Add print! and println! macros

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -20,10 +21,7 @@ pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
 
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-
-
+    println!("Hello World{}", "!");
+    
     loop {}
 }
