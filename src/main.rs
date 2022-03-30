411d4522dbcfabed5f16680f810eba97070c014b
Razieh Behjati <razieh@google.com>
2022-03-30 18:58:37 +0100

    Attempt at testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+mod serial;
 mod vga_buffer;
 
 /// This function is called on panic.
@@ -22,6 +26,40 @@ pub extern "C" fn _start() -> ! {
     // named `_start` by default
 
     println!("Hello World{}", "!");
-    
+
+    #[cfg(test)]
+    test_main();   
+ 
     loop {}
+}
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
